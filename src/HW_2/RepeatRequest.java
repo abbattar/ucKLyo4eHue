f8c8a8e869d3c4af0cbb3fa77e71e10aa5233166
abbattar <abbattar@mail.ru>
2023-04-27 09:08:07 +0300

    2commit

--- src/HW_2/RepeatRequest.java
+++ src/HW_2/RepeatRequest.java
@@ -9,17 +9,19 @@ public class RepeatRequest {
         repeatRequest();
     }
 
-    private static float  repeatRequest() {
-        Scanner sc = new Scanner(System.in);
+    private static float repeatRequest() {
+        Scanner sc;
         float fl;
         try {
-        System.out.println("Введите дробное число: ");
-        fl = sc.nextFloat();
+            sc = new Scanner(System.in);
+            System.out.println("Введите дробное число: ");
+            fl = sc.nextFloat();
         } catch (InputMismatchException e1) {
-            System.out.println("Нужно ввести число! Вводите: ");
-            fl = new Scanner(System.in).nextFloat();
-        } finally {
-            sc.close();
+            do {
+                sc = new Scanner(System.in);
+                System.out.println("Нужно ввести число! Вводите: ");
+            } while (!sc.hasNextFloat());
+            fl = sc.nextFloat();
         }
         return fl;
     }
